2766caf8153d2689179d4c7cf81e406a9c1c3303
spacedawwwg <spacedawwwg@gmail.com>
2023-03-27 10:52:41 +0100

    3 files modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-import htmlBeautify from 'astro-html-beautifier';
 
-// https://astro.build/config
 export default defineConfig({
-  // Enable React to support React JSX components.
-  integrations: [vue(), htmlBeautify()],
+  integrations: [vue()],
 });
